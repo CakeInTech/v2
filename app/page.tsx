1fdce126d3d21ba533fc265946bf4fab3b3c0756
Mohamed Ibrahim <abdulhakimm280@gmail.com>
2025-10-29 02:20:23 +0300

    Add hero background theme using aceternity ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/Hero";
+
 const page = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px10 px-5">
       <div className="max-w-7xl w-full">
-        <h1 className="text-purple">Portfolio Attack!!!</h1>
+        <Hero />
       </div>
     </main>
   );
